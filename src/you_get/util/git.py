0b888c2fdcd00670f03c1489408010814f9917e2
Mort Yao <mort.yao@gmail.com>
2014-07-05 19:01:29 +0200

    fix you_get.util.git

--- src/you_get/util/git.py
+++ src/you_get/util/git.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import os
+
 def get_head(repo_path):
     """Get (branch, commit) from HEAD of a git repo."""
     ref = open(os.path.join(repo_path, '.git', 'HEAD'), 'r').read().strip()[5:].split('/')
